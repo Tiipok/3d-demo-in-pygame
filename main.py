94f742adef6f02ee75ada64988ec21e104dbaa0b
Tipok <60702813+Tiipok@users.noreply.github.com>
2023-03-10 17:40:00 +0300

    stable ver

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+
 import pygame as p
 from copy import deepcopy
 import numpy as np
 from numba import jit
 
+# things for pygame
 H = 600
 W = 800
 disp = p.display.set_mode((W, H))
 FPS = 60
+
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+# for fps printing
 p.font.init()
 clock = p.time.Clock()
-p.display.set_caption('3d demo')
+p.display.set_caption('3d demo') 
 font = p.font.SysFont('ubuntu mono', 20)
-
 
 def createMat():
     return np.zeros((4, 4), dtype=np.float32)
@@ -218,151 +221,15 @@ def matrixRotationY(angle):
     return mat
 
 
-def matrix_Inverse(m):
-    mat = np.zeros((4, 4))
-    mat[0][0] = m[0][0]
-    mat[0][1] = m[1][0]
-    mat[0][2] = m[2][0]
-    
-
-    mat[1][0] = m[0][1]
-    mat[1][1] = m[1][1]
-    mat[1][2] = m[2][1]
-    
-    
-    mat[2][0] = m[0][2]
-    mat[2][1] = m[1][2]
-    mat[2][2] = m[2][2]
-    
-
-    mat[3][0] = -(m[3][0] * mat[0][0] + m[3][1] * mat[1][0] + m[3][2] * mat[2][0])
-    mat[3][1] = -(m[3][0] * mat[0][1] + m[3][1] * mat[1][1] + m[3][2] * mat[2][1])
-    mat[3][2] = -(m[3][0] * mat[0][2] + m[3][1] * mat[1][2] + m[3][2] * mat[2][2])
-    mat[3][3] = 1
-
-    return mat
-
-
-def matrix_PointAt(pos, target, up):
-
-    newForward = createVector()
-    newForward = vector_sub(target, pos)
-    newForward = vector_norm(newForward)
-
-    a = createVector()
-    a = vector_mul(newForward, vector_dotProduct(up, newForward))
-
-    newUp = createVector()
-    newUp = vector_sub(up, a)
-    newUp = vector_norm(newUp)
-
-    newRight = createVector()
-    newRight = vector_crossProd(newUp, newForward)
-
-    mat = np.zeros((4, 4))
-
-    mat[0][0] = newRight[0]
-    mat[0][1] = newRight[1]
-    mat[0][2] = newRight[2]
-    
-
-    mat[1][0] = newUp[0]
-    mat[1][1] = newUp[1]
-    mat[1][2] = newUp[2]
-    
-
-    mat[2][0] = newForward[0]
-    mat[2][1] = newForward[1]
-    mat[2][2] = newForward[2]
-
-    mat[3][0] = pos[0]
-    mat[3][1] = pos[1]
-    mat[3][2] = pos[2]
-    mat[3][3] = 1
-
-    return mat
-
-
-def Vector_IntersectPlane(plane_p,plane_n,lineStart, lineEnd):
-    plane_n = vector_norm(plane_n)
-    plane_d = -vector_dotProduct(lineStart, plane_n) #perhaps will be error, then just create every vector separatly
-    ab = vector_dotProduct(lineStart, plane_n)
-    bd = vector_dotProduct(lineEnd, plane_n)
-    t = (-plane_d - ab) / (bd - ab)
-    lineStartToEnd = vector_sub(lineEnd, lineStart)
-    lineToIntersect = vector_mul(lineStartToEnd, t)
-    return vector_add(lineStart, lineToIntersect)
-
-def Triangle_ClipAgainstPlane(plane_p, plane_n, in_tri, out_tri1, out_tri2):
-    plane_n = vector_norm(plane_n)
-    def dist(p):
-        n = vector_norm(p)
-        return (plane_n[0] * p[0] + plane_n[1] * p[1] + plane_n[2] * p[2] - vector_dotProduct(plane_n, plane_p))
-    
-    nInsidePointCount = 0
-    nOutsidePointCount = 0
-    inside_points = [createVector(),createVector(),createVector()]
-    outside_points = [createVector(),createVector(),createVector()]
-
-    d0 = dist(in_tri[0])
-    d1 = dist(in_tri[1])
-    d2 = dist(in_tri[2])
-    if d0 >= 0: 
-        inside_points[nInsidePointCount] = in_tri[0]
-        nInsidePointCount += 1
-    else:
-        outside_points[nOutsidePointCount] = in_tri[0]
-        nOutsidePointCount += 1
-    if d1 >= 0: 
-        inside_points[nInsidePointCount] = in_tri[1]
-        nInsidePointCount += 1
-    else:
-        outside_points[nOutsidePointCount] = in_tri[1]
-        nOutsidePointCount += 1
-    if d2 >= 0: 
-        inside_points[nInsidePointCount] = in_tri[2]
-        nInsidePointCount += 1
-    else:
-        outside_points[nOutsidePointCount] = in_tri[2]
-        nOutsidePointCount += 1
-    
-
-    if nInsidePointCount == 0:
-        return 0
-    if nInsidePointCount == 3:
-        out_tri1 = in_tri
-        return 1
-    if  nInsidePointCount == 1 and nOutsidePointCount == 2:
-        out_tri1 = in_tri
-        out_tri1[0] = inside_points[0]
-        out_tri1[1] = Vector_IntersectPlane(plane_p, plane_n, inside_points[0], outside_points[0])
-        out_tri1[2] = Vector_IntersectPlane(plane_p, plane_n, inside_points[0], outside_points[1])
-        return 1
-    if nInsidePointCount == 2 and nOutsidePointCount == 1:
-        out_tri1 = in_tri
-        out_tri2 = in_tri
-
-        out_tri1[0] = inside_points[0]
-        out_tri1[1] = inside_points[1]
-        out_tri1[2] = Vector_IntersectPlane(plane_p, plane_n, inside_points[0], outside_points[0])
-
-        out_tri2[0] = inside_points[1]
-        out_tri2[1] = out_tri1[2]
-        out_tri2[2] = Vector_IntersectPlane(plane_p, plane_n, inside_points[1], outside_points[0])
-
-        return 2
-
 def main():
+
+    mesh = get_mesh('videoship') # <- name of the obj file to load mesh
+
+    # constants and variables
     fTheta = 0
-    fYaw = 0
-    mesh = get_mesh('axis')
 
     vCamera = createVector()
-    
-    vLookDir = createVector(x=0,y=1,z=1)
-
     triProj = createTriangle()
-
     matProj = matrix_makeProj()
 
     trianglesToRaster = list()
@@ -372,66 +239,30 @@ def main():
         for event in p.event.get():
             if event.type == p.QUIT:
                 exit()
-            if event.type == p.KEYDOWN:
-                
-                vForward = vector_mul(vLookDir, 0.08*clock.get_time())
-
-                if event.key == p.K_DOWN:
-                    vCamera[1] -= 0.02 * clock.get_time()
-                if event.key == p.K_UP:
-                    vCamera[1] += 0.02 * clock.get_time()
-                if event.key == p.K_RIGHT:
-                    vCamera[0] += 0.02 * clock.get_time()
-                if event.key == p.K_LEFT:
-                    vCamera[0] -= 0.02 * clock.get_time()
-
-
-                if event.key == ord('s'):
-                    vCamera = vector_sub(vCamera, vForward)
-                if event.key == ord('w'):
-                    vCamera = vector_add(vCamera, vForward)
-                if event.key == ord('d'):
-                    fYaw += 0.02 * clock.get_time()
-                if event.key == ord('a'):
-                    fYaw -= 0.02 * clock.get_time()
-                
-
-
 
         disp.fill(black)
-        
-        matTrans = matrix_makeTranslation(0, 5, 5)
 
-        matWorld = createIdentity()
-        matWorld = np.matmul(matWorld, matTrans)
+        # rotating over z and x
+        fTheta += 0.001 * clock.get_time()
+        matRotZ = matrixRotationZ(fTheta)
+        matRotX = matrixRotationX(fTheta * 0.5)
 
-        vTarget = createVector(x=0,y=1,z=1)
-        vUp = createVector(x=0,y=1,z=0)
-        matCameraRot = matrixRotationY(fYaw)
-        vLookDir = matrixVector_Mul(matCameraRot, vTarget)
+        # adding offset to z
+        matTrans = matrix_makeTranslation(0, 0, 8)# <- 15 is just enough for videoship and axis(for teapot 7 is fine)
 
+        # creating worls matrix
+        matWorld = matrixMatrix_Mul(matRotZ, matRotX)
+        matWorld = matrixMatrix_Mul(matWorld, matTrans)
 
-        vTarget = vector_add(vCamera, vLookDir)
-        
-        matCamera = matrix_PointAt(vCamera, vTarget, vUp)
-
-
-        matVeiw = matrix_Inverse(matCamera)
-
-
-
-
+        # calculating triangles
         for tri in mesh:
-
-            triViewed = createTriangle()
 
             tri0 = createVector(x=tri[0][0], y=tri[0][1], z=tri[0][2])
             tri1 = createVector(x=tri[1][0], y=tri[1][1], z=tri[1][2])
             tri2 = createVector(x=tri[2][0], y=tri[2][1], z=tri[2][2])
 
             triTransformed = createTriangle()
-
-
+            
             triTransformed[0] = matrixVector_Mul(matWorld, tri0)
             triTransformed[1] = matrixVector_Mul(matWorld, tri1)
             triTransformed[2] = matrixVector_Mul(matWorld, tri2)
@@ -443,22 +274,19 @@ def main():
 
             vCameraRay = vector_sub(triTransformed[0], vCamera)
 
+            # testing to draw triangle
             if vector_dotProduct(vCameraRay, normal) < 0:
+
+                # placing the light
                 light_direction = createVector(x=0, y=1, z=-1)
                 light_direction = vector_norm(light_direction)
 
                 dp = max(0.1, vector_dotProduct(light_direction, normal))
 
-                triViewed[0] = matrixVector_Mul(matVeiw, triTransformed[0])
-                triViewed[1] = matrixVector_Mul(matVeiw, triTransformed[1])
-                triViewed[2] = matrixVector_Mul(matVeiw, triTransformed[2])
+                triProj[0] = matrixVector_Mul(matProj, triTransformed[0])
+                triProj[1] = matrixVector_Mul(matProj, triTransformed[1])
+                triProj[2] = matrixVector_Mul(matProj, triTransformed[2])
 
-                #3D -> 2D
-                triProj[0] = matrixVector_Mul(matProj, triViewed[0])
-                triProj[1] = matrixVector_Mul(matProj, triViewed[1])
-                triProj[2] = matrixVector_Mul(matProj, triViewed[2])
-
-                #Scaling
                 triProj[0] = vector_div(triProj[0], triProj[0][3])
                 triProj[1] = vector_div(triProj[1], triProj[1][3])
                 triProj[2] = vector_div(triProj[2], triProj[2][3])
@@ -477,24 +305,26 @@ def main():
                 triProj[2][1] *= 0.5 * H
 
                 triProj[3][0] = dp
-
                 append(deepcopy(triProj))
 
+        # sorting triangles from bottom to top
         trianglesToRaster.sort(reverse=True, key=Sort)
 
+        # drawing sorted triangles
         for tr in trianglesToRaster:
-            FillTriangle(tr, tr[3][0], [255, 255, 255])
+            color = [255, 255, 255]
+            FillTriangle(tr, tr[3][0], color)
 
         trianglesToRaster.clear()
 
-        #printing fps
-        text = font.render(str(clock.get_fps())[:5], True, white)
+
+        text = font.render(str(int(clock.get_fps())), True, white)
         place = text.get_rect(center=(20, 20))
         disp.blit(text, place)
+
         p.display.update()
         clock.tick(FPS)
 
 
 if __name__ == '__main__':
     main()
-
